83773c568a7e5babf262f1a114565fd8420206ea
Savatia <briankeyonzo@yahoo.com>
2015-08-13 01:47:27 +0300

    Finished password resets and account activations- Fixed two minor bugs

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -29,7 +29,7 @@ class PasswordResetsController < ApplicationController
     elsif @user.update_attributes(user_params)
       log_in @user
       flash[:success] = "Password has been reset."
-      redirect_to @user
+      redirect_to user_path(@user.name)
     else
       render 'edit'
     end
